e0e09c613c5301826965bedfd028b027163b63d0
Z <kerenyi.nagy.zoltan@gmail.com>
2019-11-03 20:53:05 +0100

    Update caesar.py

--- caesars_cypher/caesar.py
+++ caesars_cypher/caesar.py
@@ -1,18 +1,18 @@
 def caesar_encrypt(word, step):
     if not type(word) is str:
-        raise TypeError("\'word\' argument must be a string")
+        raise TypeError("'\'word\' argument must be a string")
     if not type(step) is int:
         raise TypeError("\'step\' argument must be an integer")
-    print(word)
-    result = ""
+    
+    encrypted_word = ''
     for char in word:
         if char is ' ':
-            result += ' '
-        if char.isupper():
-            result += chr((ord(char) + step - 65) % 26 + 65)
+            encrypted_word += ' '
+        elif char.isupper():
+            encrypted_word += chr((ord(char) + step - 65) % 26 + 65)
         else:
-            result += chr((ord(char) + step - 97) % 26 + 97)
-    print(result)
+            encrypted_word += chr((ord(char) + step - 97) % 26 + 97)
+    print(encrypted_word)
 
 
 caesar_encrypt("treaty impossible", 3)
